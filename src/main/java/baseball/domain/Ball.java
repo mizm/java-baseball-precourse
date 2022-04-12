eb845810db6c2a935c41489cb154b1881e1fc07f
marty.ni <marty.ni@kakaocorp.com>
2022-04-12 20:19:11 +0900

    Feat : Ball 문자열로 생성 기능 추가

--- src/main/java/baseball/domain/Ball.java
+++ src/main/java/baseball/domain/Ball.java
@@ -6,7 +6,9 @@ public class Ball {
 
     private static final int MIN = 1;
     private static final int MAX = 9;
-    private static final String NUMBER_RANGE_VALIDATION_MESSAGE = "공의 숫자 범위는 1~9 입니다.";
+    private static final String NUMBER_RANGE_EXCEPTION_MESSAGE = "공의 숫자 범위는 1~9 입니다.";
+    private static final String NOT_NUMBER_EXCEPTION_MESSAGE = "입력 값이 숫자가 아닙니다.";
+    private static final String EMPTY_EXCEPTION_MESSAGE = "입력 값은 빈 값 일 수 없습니다.";
     private static final Ball[] ZERO_THROUGH_NINE = {
             new Ball(0),
             new Ball(1),
@@ -26,15 +28,30 @@ public class Ball {
         this.number = number;
     }
 
-    private static void validateNumber(int number) {
+    private static void validateNumberRange(int number) {
         if (number < MIN || number > MAX) {
-            throw new IllegalArgumentException(NUMBER_RANGE_VALIDATION_MESSAGE);
+            throw new IllegalArgumentException(NUMBER_RANGE_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private static void validateNotNumber(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException(EMPTY_EXCEPTION_MESSAGE);
+        }
+
+        if (!input.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException(NOT_NUMBER_EXCEPTION_MESSAGE);
         }
     }
 
     public static Ball valueOf(int number) {
-        validateNumber(number);
+        validateNumberRange(number);
         return ZERO_THROUGH_NINE[number];
+    }
+
+    public static Ball valueOf(String number) {
+        validateNotNumber(number);
+        return valueOf(Integer.parseInt(number));
     }
 
     @Override
--- src/test/java/baseball/domain/BallTest.java
+++ src/test/java/baseball/domain/BallTest.java
@@ -2,9 +2,11 @@ package baseball.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class BallTest {
 
@@ -12,8 +14,8 @@ class BallTest {
     @DisplayName("볼을 생성한다.")
     void create() {
         Ball ball = Ball.valueOf(5);
-        assertThat(ball).isEqualTo(Ball.valueOf(5));
-        assertThat(ball == Ball.valueOf(5)).isTrue();
+        assertThat(ball).isEqualTo(Ball.valueOf("5"));
+        assertThat(ball == Ball.valueOf("5")).isTrue();
     }
 
     @Test
@@ -26,6 +28,20 @@ class BallTest {
         assertThatThrownBy(() -> {
             Ball.valueOf(10);
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("숫자 이외의 값이 오면 IllegalArgumentException")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "한", "-", "/"})
+    void notNumberTest(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() -> Ball.valueOf(input));
+    }
+
+    @DisplayName("null or empty 값이 오면 IllegalArgumentException")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void notEmptyTest(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() -> Ball.valueOf(input));
     }
 
 
